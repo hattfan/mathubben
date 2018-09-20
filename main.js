5802c61ef969ed1c6322fd74d2b171564c14ac34
Ola Karlsson <ola.anders.karlsson@gmail.com>
2018-09-20 10:47:21 +0200

    uppdaterat konkurrentanalysen

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ app.use(function (req, res, next) {
     next();
 });
 
-MongoClient.connect('mongodb://ola:Neroxrox5(@ds235732.mlab.com:35732/mathubben', (err, client) => {
-// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+// MongoClient.connect('mongodb://ola:Neroxrox5(@ds235732.mlab.com:35732/mathubben', (err, client) => {
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
 
 // var db = client.db('statistik');
 var db = client.db('mathubben');
@@ -48,6 +48,12 @@ app.get('/varugrupptest/:grupp',  function (req, res) {
 		res.json(data)
 	})
 });
+
+app.get('/produkter/:id', function(req, res) {
+	db.collection('produkter').find({'LevArtNr':req.params.id}).toArray(function(err, res){
+			console.log(res)
+	})
+})
 
 app.get('/konkurrentanalys', function (req, res) {
 	res.render('konkurrentanalys.ejs');
@@ -85,9 +91,8 @@ app.get('/getmongodata/:type', function (req, res) {
 	MongoClient.connect('mongodb://localhost:27017', (err, client) => {
 		var db = client.db('dabas');
 
-		// console.log('Databas-route till: ' + req.params.type)
 		if (err) throw err;
-		// req.params.type
+
 		db.collection("menigoRep4").find().toArray(function (err, res) {
 			for (let i = 0; i < res.length; i++) {
 				temp.push(res[i])
@@ -108,32 +113,26 @@ app.get('/alldbs/:type', function (req, res) {
 	MongoClient.connect('mongodb://localhost:27017', (err, client) => {
 		var db = client.db('dabas');
 
-		// console.log('Databas-route till: ' + req.params.type)
 		if (err) throw err;
-		// req.params.type
 		db.listCollections().toArray(function (err, data) {
 			for (let i = 0; i < data.length; i++) {
 				temp.push(data[i].name)
 			}
-			// res.render('export.ejs', { dbs : temp });
 
 			var uppfukkat = db.collection(req.params.type);
 			uppfukkat.find().toArray(function (err, dbData) {
-				// console.log(dbData[0])
 				var tempDB = [];
 				for (let i = 0; i < dbData.length; i++) {
 					tempDB.push(dbData[i])
 				}
-				// console.log(tempDB)
+
 				res.render('export.ejs', { dbs: temp, dbDataExp: dbData });
-				// 
 			})
 		})
 	})
 })
 
 })
-// var portSettings = process.env.PORT
 var port = process.env.PORT || 3030; 
 
 app.listen(port, process.env.IP, function () {
@@ -141,100 +140,3 @@ app.listen(port, process.env.IP, function () {
 	appConsoleMsg += process.env.IP + ':' + port;
 	console.log(appConsoleMsg);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //????????????????????????????????????????????????????????????????????????????????????
-// //??? REST get data ???????????????????????????????????????????????????????????????????????
-// //????????????????????????????????????????????????????????????????????????????????????
-// app.get('/dbyear2/:type', function (req, res) {
-// 	var befolkning = require('./views/js/kommundata.js')
-// 	kommunVikt = {};
-	
-
-// 		var years = ['2015','2016','2017']
-// 		db.collection("combinedDBs").distinct("KommunNummer", function (err, kommunData) {
-
-// 			if (err) throw err
-// 			//Loopar över kommunnumerna
-// 			for (let i = 0; i < kommunData.length; i++) {
-// 				const kommun = kommunData[i];
-
-// 				kommunVikt[kommun] = {
-// 					2015: {
-// 						'Mängd':0,
-// 						'Kronor':0},
-// 					2016: {
-// 						'Mängd':0,
-// 						'Kronor':0},
-// 					2017: {
-// 						'Mängd':0,
-// 						'Kronor':0},
-// 				}
-// 			}
-
-// 			//Letar upp valda grupper i databasen
-// 			db.collection("combinedDBs").find({ 'VarugruppMathubben': req.params.type }).toArray(function (err, mathubbenStatistik) {
-// 				if (err) throw err;
-
-// 				for (let i = 0; i < mathubbenStatistik.length; i++) {
-// 					//Loopear över kommunnummerna och letar om de finns i mathubbenstatistiken
-// 					if (mathubbenStatistik[i]['KommunNummer'] in kommunVikt) {
-// 						vikt = mathubbenStatistik[i]['Mängd'];
-// 						kronor = mathubbenStatistik[i]['Kronor'];
-// 						//vikt countar över mängden i varje statistik-rad
-// 						if(mathubbenStatistik[i]['År'] === 2015){
-// 							kommunVikt[mathubbenStatistik[i]['KommunNummer']][2015]['Mängd'] = kommunVikt[mathubbenStatistik[i]['KommunNummer']][2015]['Mängd'] + Math.round((vikt * 100) / 100);
-// 							kommunVikt[mathubbenStatistik[i]['KommunNummer']][2015]['Kronor'] = kommunVikt[mathubbenStatistik[i]['KommunNummer']][2015]['Kronor'] + Math.round((kronor * 100) / 100);
-// 						} 
-// 						else if (mathubbenStatistik[i]['År'] === 2016){
-// 							kommunVikt[mathubbenStatistik[i]['KommunNummer']][2016]['Mängd'] = kommunVikt[mathubbenStatistik[i]['KommunNummer']][2016]['Mängd'] + Math.round((vikt * 100) / 100);
-// 							kommunVikt[mathubbenStatistik[i]['KommunNummer']][2016]['Kronor'] = kommunVikt[mathubbenStatistik[i]['KommunNummer']][2016]['Kronor'] + Math.round((kronor * 100) / 100);
-// 						}
-// 						else if (mathubbenStatistik[i]['År'] === 2017){
-// 							kommunVikt[mathubbenStatistik[i]['KommunNummer']][2017]['Mängd'] = kommunVikt[mathubbenStatistik[i]['KommunNummer']][2017]['Mängd'] + Math.round((vikt * 100) / 100);
-// 							kommunVikt[mathubbenStatistik[i]['KommunNummer']][2017]['Kronor'] = kommunVikt[mathubbenStatistik[i]['KommunNummer']][2017]['Kronor'] + Math.round((kronor * 100) / 100);
-// 						}
-
-// 						vikt = "";
-// 					}
-// 				}
-// 				var final = []
-// 				var temp = {}
-// 				for (const key in kommunVikt) {
-// 					years.forEach(year => {
-// 						// console.log(kommunVikt[key][year])
-// 						temp = { 
-// 							'KommunNummer' : key,
-// 							'Year' : year,
-// 							'MängdTotal' : kommunVikt[key][year]['Mängd'],
-// 							'KronorTotal' : kommunVikt[key][year]['Kronor'],
-// 							'MängdPerCapita': kommunVikt[key][year]['Mängd']/befolkning[key],
-// 							'KronorPerCapita' : kommunVikt[key][year]['Kronor']/befolkning[key]
-
-// 						}
-// 						final.push(temp)
-// 						temp = {}
-// 					})
-// 				}
-// 				res.json(final)
-// 				// {continent: "Asia", country: "Afghanistan", countryCode: "004", emissions: 4217.05, emissionsPerCapita: 0.156001008723042}
-
-// 			})
-// 		})
-// 	})
---
+++ views/js/konkurrentanalys/map_konkurrentanalys.js
@@ -0,0 +1,82 @@
+function createMap(width, height) {
+  d3.select("#map")
+    .attr("width", width)
+    .attr("height", height*2+50)
+}
+
+function drawMap(geoData, kommunData, year, dataType, calculationType, produktgrupp) {
+  var visningsVal = dataType + calculationType
+  var map = d3.select('#map')
+
+  var projection = d3.geoMercator()
+    .scale(1100)
+    .translate([0, 1900])
+    // .scale(4300)
+    // .translate([-700, 6300])
+    //If mobile
+    // .scale(4200)
+    // .translate([-1000, 5400])
+
+  var path = d3.geoPath()
+    .projection(projection);
+  
+  geoData.forEach(d => {
+    var kommuner = kommunData.filter(row => row.KommunNummer === d.properties.KNKOD);
+    var name = '';
+    //Koppla ihop geoData med kommunData mha kommunkod
+    if (kommuner.length > 0) name = kommuner[0].kommun;
+    d.properties.data = kommuner.find(kommun => +kommun.Year === year) || { kommun: name };
+
+  });
+
+  var colors = ["#7caeff", "#ff0400"];
+
+  var domain = [0, d3.max(kommunData, d => d[visningsVal])];
+
+  var mapColorScale = d3.scaleLinear()
+    .domain(domain)
+    .range(colors);
+
+  var addG = map.append("g")
+
+  var zoom_handler = d3.zoom()
+    .on("zoom", zoom_actions);
+
+  //specify what to do when zoom event listener is triggered 
+  zoom_handler(addG);
+
+  var update = addG.selectAll(".kommun")
+    .data(geoData);
+
+  update
+    .enter()
+    .append("path")
+    .classed("kommun", true)
+    .attr("d", path)
+    .on("click", function () {
+      var kommun = d3.select(this);
+      var isActive = kommun.classed("active");
+      var kommunName = isActive ? "" : kommun.data()[0].properties['KNNAMN'];
+      var kommunKod = isActive ? "" : kommun.data()[0].properties['KNKOD'];
+
+      var visningsVal = dataType + calculationType
+      drawBar(kommunData, kommunKod, visningsVal)
+      highlightBars(+d3.select("#year").property("value"));
+      d3.selectAll(".kommun").classed("active", false);
+      kommun.classed("active", !isActive);
+    })
+    .merge(update)
+      .transition()
+      .duration(750)
+      .attr("fill", d => {
+        var val = d.properties.data[visningsVal];
+        return val ? mapColorScale(val) : "#ccc";
+      });
+
+  function zoom_actions(){
+    addG.attr("transform", d3.event.transform);
+  }
+  
+};
+
+
