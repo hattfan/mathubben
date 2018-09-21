fe2a8fb974be52efb10dd9392287059a3e795a6a
Ola Karlsson <ola.anders.karlsson@gmail.com>
2018-09-21 08:38:55 +0200

    ändrat server

--- main.js
+++ main.js
@@ -28,8 +28,11 @@ app.use(function (req, res, next) {
     next();
 });
 
-// MongoClient.connect('mongodb://ola:Neroxrox5(@ds235732.mlab.com:35732/mathubben', (err, client) => {
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+// if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {console.log('localhost')}
+
+MongoClient.connect('mongodb://ola:Neroxrox5(@ds235732.mlab.com:35732/mathubben', (err, client) => {
+// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+
 
 // var db = client.db('statistik');
 var db = client.db('mathubben');
